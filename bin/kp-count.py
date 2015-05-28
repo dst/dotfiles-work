f63464ea32791228b692872ec620bc5cf31f1552
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-05-29 00:10:37 +0200

    Small refactorings in kp script

--- bin/kp-count.py
+++ bin/kp-count.py
@@ -9,10 +9,15 @@ import datetime
 import os
 import sys
 
-WORK_DAY_IN_SECS = 8 * 60 * 60
+HOUR_IN_SECS = 60 * 60
+WORK_DAY_IN_SECS = 8 * HOUR_IN_SECS
 
 def usage():
    print "Usage: %s file" % sys.argv[0]
+
+def exit_with_msg(msg):
+    print msg
+    sys.exit(1)
 
 def get_kp_file():
     if len(sys.argv) != 2:
@@ -29,9 +34,12 @@ def get_durations(kp_file):
         if not line or line.startswith("#"):
             continue
         # dd.mm.yyyy: hh.mm - hh.mm
-        start, end = map(str2time, line.split(":")[1].split("-"))
+        tokens = line.split(":")
+        if len(tokens) != 2:
+            exit_with_msg("Incorrect line: %s" % line)
+        start, end = map(str2time, tokens[1].split("-"))
         dummydate = datetime.date(2000,1,1)
-        duration = datetime.datetime.combine(dummydate,  end) - datetime.datetime.combine(dummydate, start)
+        duration = calculate_duration(start, end)
         durations.append(duration)
     return durations
 
@@ -40,35 +48,42 @@ def str2time(str):
         hours, minutes = str.split(".")
         return datetime.time(int(hours), int(minutes))
     except ValueError:
-        print "Cannot parse time%s" % str
-        sys.exit(1)
+        exit_with_msg("Cannot parse time%s" % str)
+
+def calculate_duration(start, end):
+    dummydate = datetime.date(2000,1,1)
+    return datetime.datetime.combine(dummydate,  end) - datetime.datetime.combine(dummydate, start)
 
 def ornament(duration):
     if duration.seconds > WORK_DAY_IN_SECS:
         return "+"
-    if duration.seconds < WORK_DAY_IN_SECS:
+    elif duration.seconds < WORK_DAY_IN_SECS:
         return "-"
-    return ""
+    else:
+        return ""
 
 def seconds2time(seconds):
     seconds = abs(seconds)
-    hours = seconds / (60 * 60)
-    minutes = (seconds - hours * 60 * 60) / 60
+    hours = seconds / HOUR_IN_SECS
+    minutes = (seconds - hours * HOUR_IN_SECS) / 60
     return "%dh %dm" % (hours, minutes)
 
 def diff_prefix(diff):
     return "positive" if diff >= 0 else "negative"
 
-kp_file = get_kp_file()
-print "Counting hours in %s" % kp_file
-durations = get_durations(kp_file)
+def main():
+    kp_file = get_kp_file()
+    print "Counting hours in %s" % kp_file
 
-sum = 0
-diff = 0
-for d in durations:
-    print "%s %s" % (seconds2time(d.seconds), ornament(d))
-    sum += d.seconds
-    diff += (d.seconds - WORK_DAY_IN_SECS)
-print "----------------"
-print "TOTAL: %s" % seconds2time(sum)
-print "%s diff: %s" % (diff_prefix(diff), seconds2time(diff))
+    sum = 0
+    diff = 0
+    for d in get_durations(kp_file):
+        print "%s %s" % (seconds2time(d.seconds), ornament(d))
+        sum += d.seconds
+        diff += (d.seconds - WORK_DAY_IN_SECS)
+    print "----------------"
+    print "TOTAL: %s" % seconds2time(sum)
+    print "%s diff: %s" % (diff_prefix(diff), seconds2time(diff))
+
+if __name__ == "__main__":
+    main()
